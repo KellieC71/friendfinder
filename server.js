ac36909629cebfd49c69eb09cf97fbd7a96e36b0
kellie Cuitino <kelliecuitino@Kellies-MacBook-Pro.local>
2018-12-07 20:50:56 -0500

    friend finder

--- server.js
+++ server.js
@@ -11,3 +11,74 @@ var PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// =============================================================
+var characters = [
+  {
+    routeName: "emma",
+    name: "Emma",
+    photo: "", 
+    answers: [1, 2, 3, 4, 5]
+  },
+  {
+    routeName: "kellie",
+    name: "Kellie",
+    photo: "",
+    answers: [3, 4, 1 ,2 ,3 ]
+  },
+];
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "survey.html"));
+});
+
+app.get("/add", function(req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+// Displays all characters
+app.get("/api/friends", function(req, res) {
+  return res.json(friends);
+});
+
+// Displays a single character, or returns false
+app.get("/api/friends/:name", function(req, res) {
+  var chosen = req.params.name;
+
+  console.log(chosen);
+
+  for (var i = 0; i < friends.length; i++) {
+    if (chosen === friends[i].routeName) {
+      return res.json(friends[i]);
+    }
+  }
+
+  return res.json(false);
+});
+
+// Create New Characters - takes in JSON input
+app.post("/api/friends", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  var newfriend = req.body;
+
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  newfriend.routeName = newfriend.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newfriend);
+
+  friends.push(newfriend);
+
+  res.json(newfriend);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
